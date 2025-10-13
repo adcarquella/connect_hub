ca752fb2e9137fd88047756c32b04d4517cc1088
adcarquella <110229601+adcarquella@users.noreply.github.com>
2025-10-13 16:25:52 +0100

    Updated api path

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { encryptPayload, decryptPayload } from "./crypto";
 
 //const API_BASE = "http://localhost:3000"; // backend URL
-const API_BASE = "https://connecthubapi.arquel.la"; // backend URL
+const API_BASE = "https://connect.arquella.co.uk"; // backend URL
 
 const ENCRYPTION_KEY_BASE64 = "your_base64_key_here"; // same key as backend
 
